a607985ff7afc54c6c2f7ced353095f54649e18c
kuyilraj1-commits <kuyilraj1@gmail.com>
2026-07-02 22:10:48 +0530

    final commit

---
+++ _tanstack-start-manifest_v-BiZrLVqP.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-BiZrLVqP.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/ELCOT/Desktop/luminous-edit-render-main/luminous-edit-render-main/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-CLHd4cYt.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CLHd4cYt.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/ELCOT/Desktop/luminous-edit-render-main/luminous-edit-render-main/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DE-aci0U.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
